05663f7504c098b55e4fc143a5842add965f3c4a
Abdul Haseeb <abdulhaseeb1652@gmail.com>
2025-06-27 17:10:27 +0500

    Update user.routes.js

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -11,15 +11,12 @@ import { verifyToken } from '../utils/verifyUser.js';
 
 const authRouter = express.Router();
 
-// 🔓 Public Routes
 authRouter.post('/signup', signup);
 authRouter.post('/signin', signin);
 authRouter.post('/google', google);
 
-// 🚪 Logout Route
 authRouter.get('/signout', logout);
 
-// 🔐 Protected Routes
 authRouter.put('/update/:id', verifyToken, updateUser);
 authRouter.delete('/delete/:id', verifyToken, deleteUser);
 
